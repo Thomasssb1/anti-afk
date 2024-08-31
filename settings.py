b63aad7c265f43e30f838844b00d31ee47c1469f
Thomasssb1 <thomas.beer04@outlook.com>
2024-08-31 15:24:20 +0100

    Update interval to allow for seconds, minutes, hours

--- settings.py
+++ settings.py
@@ -1,24 +1,46 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from subprocess import Popen, PIPE
 import pygetwindow as gw
 
 
 class Settings:
-    def __init__(self, programName, interval, keyToStop):
-        self.interval = interval
+    def __init__(self, interval, keyToStop, position):
+        self.window = None
+        self.interval = Settings.parseInterval(interval)
         self.key = keyToStop
-        self.programName = programName
+        self.position = Settings.parsePosition(position)
         self.lastUsed = datetime.now()
 
+    def parsePosition(position):
+        if position == None:
+            return
+        try:
+            newPosition = tuple(map(int, position.split(",")))
+            return newPosition
+        except:
+            raise Exception("Invalid position format")
+
+    def parseInterval(interval):
+        try:
+            symbol = str.lower(interval[-1])
+            newInterval = int(interval[:-1])
+            if symbol == "s":
+                return timedelta(seconds=newInterval)
+            elif symbol == "m":
+                return timedelta(minutes=newInterval)
+            elif symbol == "h":
+                return timedelta(hours=newInterval)
+            else:
+                raise Exception("Invalid interval format")
+        except:
+            raise Exception("Invalid interval format")
+
     def checkInterval(self):
-        if (datetime.now() - self.lastUsed).total_seconds() // 60 >= self.interval:
+        if (datetime.now() - self.lastUsed) >= self.interval:
             return True
 
     def updateLastUsed(self):
         self.lastUsed = datetime.now()
 
-    def findWindow(self):
-        window = gw.getWindowsWithTitle(self.programName)
-        if len(window) == 0:
-            raise Exception("No window found with the given name")
-        return window[0]
+    def setWindow(self, window):
+        self.window = window
