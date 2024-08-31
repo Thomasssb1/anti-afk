cf64ca4bc5f2ea7e718f88f7c47428b7b4a7edc3
Thomasssb1 <thomas.beer04@outlook.com>
2024-08-31 15:24:47 +0100

    Change how the target program is detected

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import sys
 import mouse
+import keyboard
+import time
 import pygetwindow as gw
 import random
 from settings import Settings
 
 
-def click(window):
+def click(settings):
+    window = settings.window
     if not window.isMaximized:
         window.restore()
 
-    mouse.move(
-        window.width / 2, window.height / 2, absolute=True, duration=random.random()
-    )
+    position = settings.position or (window.width / 2, window.height / 2)
+    x, y = window.topleft.x + position[0], window.topleft.y + position[1]
+
+    duration = random.random()
+    mouse.move(x, y, absolute=True, duration=random.random())
+    time.sleep(duration + 0.1)
 
     mouse.click("left")
+
+    window.minimize()
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-
-    if len(args) == 0:
-        raise Exception("No program name given")
-
     settings = Settings(
-        args[0],
-        len(args) >= 1 and args[1] or 15,
-        len(args) >= 2 and args[2] or "q",
+        len(args) >= 1 and args[0] or "2s",
+        len(args) >= 2 and args[1] or "q",
+        len(args) >= 3 and args[2] or None,
     )
 
-    mouse.on_click(lambda: settings.updateLastUsed())
+    print(
+        "Recording mouse position, once hovering over the program window double click to confirm the target program"
+    )
+
+    def set_window():
+        position = mouse.get_position()
+        try:
+            window = gw.getWindowsAt(position[0], position[1])[0]
+            settings.setWindow(window)
+        except:
+            raise Exception("No window found at the position")
+
+    hook = mouse.on_button(callback=set_window, buttons="left", types="double")
+    mouse.wait(button="left", target_types="double")
+    mouse.unhook(hook)
+
+    while True:
+        if keyboard.is_pressed(settings.key):
+            break
+        if settings.checkInterval():
+            click(settings)
+            settings.updateLastUsed()
